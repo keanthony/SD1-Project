ff66200c8dd5f0a224554439eab5682b6e0bb0ac
Adrianna Valdivia <adrianna.valdivia@gmail.com>
2015-10-26 10:44:15 -0400

    Adrianna's Code Commit

---
+++ Adrianna-SD1/src/NPC.java
@@ -0,0 +1,17 @@
+package Dealing With Death.Use Cases.USE CASE.NPC;
+
+public class NPC {
+
+	private boolean dead;
+
+	private String trade;
+
+	public void attack() {
+
+	}
+
+	public void die() {
+
+	}
+
+}
---
+++ Adrianna-SD1/src/Room.java
@@ -0,0 +1,25 @@
+package Dealing With Death.Use Cases.USE CASE.ROOM;
+
+import Dealing With Death.Use Cases.USE CASE.NPC.NPC;
+
+public class Room {
+
+	private String name;
+
+	private boolean isEmpty;
+
+	private String description;
+
+	private Riddle riddle;
+
+	private NPC crack;
+
+	public NPC initCrack() {
+		return null;
+	}
+
+	public String enter() {
+		return null;
+	}
+
+}
