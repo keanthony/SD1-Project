30d3e3b0be4a7a64feff0b859cf0db1db0934345
Adrianna Valdivia <adrianna.valdivia@gmail.com>
2015-11-02 10:07:07 -0500

    Changes to NPC package and ROOM Package

--- DealingWithDeath/src/NPC/NPC.java
+++ DealingWithDeath/src/NPC/NPC.java
@@ -3,14 +3,21 @@ package NPC;
 public class NPC {
 
 	private boolean dead;
-
 	private String trade;
+	private String NPC;
+	
+	public boolean isDead()
+	{
+		return dead;
+	}
 
 	public void attack() {
 
+		//this should bring down health points of the main player
 	}
 
 	public void die() {
+		
 
 	}
 
--- DealingWithDeath/src/ROOM/Riddle.java
+++ DealingWithDeath/src/ROOM/Riddle.java
@@ -1,19 +1,40 @@
 package ROOM;
 
-public class Riddle {
+import NPC.NPC;
+
+public class Riddle extends Room {
 
 	private boolean isCorrect;
+	
+	public Riddle(String name, boolean isEmpty, String description, Riddle riddle, NPC crack, boolean isCorrect) {
+		super(name, isEmpty, description, riddle, crack);
+		this.isCorrect = isCorrect;
+	}
+	
+	public boolean isCorrect() {
+		return isCorrect;
+	}
+
+	public void setCorrect(boolean isCorrect) {
+		this.isCorrect = isCorrect;
+	}
 
 	public String question() {
 		return null;
+		// if in room [1] riddle is " question 1 "
+		// else if in room [2] riddle is " questions 2"
+		// etc so on
+		// has to be at least 6 rooms, so 6 riddles
 	}
 
 	public String answer(int ans) {
 		return null;
+		// if user selects option [1,2,3,4,5] ... isCorrect or fights the devil
 	}
 
 	public Room reward(boolean isCorrect) {
 		return null;
+		
 	}
 
 }
