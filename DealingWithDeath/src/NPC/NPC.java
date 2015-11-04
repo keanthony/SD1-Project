0740bda42847d1f83eb5d5af0de4e3ed9f996543
Adrianna Valdivia <adrianna.valdivia@gmail.com>
2015-11-04 09:54:37 -0500

    committing NPC to pull other changes

--- DealingWithDeath/src/NPC/NPC.java
+++ DealingWithDeath/src/NPC/NPC.java
@@ -5,7 +5,33 @@ public class NPC {
 	private boolean dead;
 	private String trade;
 	private String NPC;
-	
+
+	public NPC(boolean dead, String trade, String nPC) {
+		this.dead = dead;
+		this.trade = trade;
+		NPC = nPC;
+	}
+
+	public String getTrade() {
+		return trade;
+	}
+
+	public void setTrade(String trade) {
+		this.trade = trade;
+	}
+
+	public String getNPC() {
+		return NPC;
+	}
+
+	public void setNPC(String nPC) {
+		NPC = nPC;
+	}
+
+	public void setDead(boolean dead) {
+		this.dead = dead;
+	}
+
 	public boolean isDead()
 	{
 		return dead;
