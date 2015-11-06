27c13bb1d1c39208b86a359892d6aa9f571d0035
Kevin Anthony <keanthony@gmail.com>
2015-11-06 10:51:03 -0500

    wrote question method in Riddle

--- DealingWithDeath/src/PLAYER/Commands.java
+++ DealingWithDeath/src/PLAYER/Commands.java
@@ -11,7 +11,7 @@ package PLAYER;
 
 public enum Commands
 {
-	attack, moveForward, moveBackward, inventory, equip,
+	attack, move, inventory, equip,
 	drop, take,	deal, sell, inspect
 	
 }
--- DealingWithDeath/src/ROOM/Riddle.java
+++ DealingWithDeath/src/ROOM/Riddle.java
@@ -1,59 +1,83 @@
 package ROOM;
 
+import MAIN.DealingWithDeath;
 import NPC.NPC;
+import PLAYER.Player;
 
-public class Riddle extends Room {
-
+public class Riddle
+{
 	private int riddleId;
 	private String question;
 	private boolean isCorrect;
-	
-	/**
-	 * @param rName
-	 * @param rDescription
-	 * @param isREmpty
-	 * @param rRiddle
-	 * @param crackHeads
+	private String riddleDesc;
+	private Room backToRoom;
+	private int solution;
+
+	/**Constructor: Riddle.java
+	 * Initializes object with following params.
+	 * @param riddleId
+	 * @param question
 	 * @param isCorrect
+	 * @param riddleDesc
+	 * @param backToRoom
+	 * @param solution
 	 */
-	public Riddle() 
+	public Riddle(int riddleId, String question, boolean isCorrect,
+			String riddleDesc, Room backToRoom, int solution)
 	{
-		
-		this.riddleId = 0;
+		this.riddleId = riddleId;
+		this.question = question;
 		this.isCorrect = false;
-	}
-	
-	public Riddle(String rName, String rDescription, boolean isREmpty, Riddle rRiddle, NPC crackHeads,
-			boolean isCorrect) 
-	{
-		super();
-		this.isCorrect = isCorrect;
+		this.riddleDesc = riddleDesc;
+		this.backToRoom = backToRoom;
+		this.solution = solution;
 	}
 
-	public boolean isCorrect() {
+	public int getSolution()
+	{
+		return solution;
+		// if user selects option [1,2,3,4,5] ... isCorrect or fights the devil
+	}
+
+	public boolean isCorrect()
+	{
 		return isCorrect;
 	}
 
-	public void setCorrect(boolean isCorrect) {
-		this.isCorrect = isCorrect;
+	public void setCorrect(boolean isCorrect)
+	{
+		isCorrect = true;
 	}
 
-	public String question() {
-		return null;
+	public String question(int answer)
+	{
+		String result = "";
+		if (!isCorrect)
+		{
+			if (answer == getSolution())
+			{
+				setCorrect(isCorrect = true);
+				result = "You answered the Riddle correctly and will be returned to your Room";
+				reward(backToRoom);
+			}
+		}
+		else
+		{
+			result = "Sorry, your answer was incorrect";
+		}
+		return result;
 		// if in room [1] riddle is " question 1 "
 		// else if in room [2] riddle is " questions 2"
 		// etc so on
 		// has to be at least 6 rooms, so 6 riddles
 	}
 
-	public String answer(int ans) {
-		return null;
-		// if user selects option [1,2,3,4,5] ... isCorrect or fights the devil
+	public void reward(Room lastRoom)
+	{
+		Player p = new Player();
+		if (isCorrect)
+		{
+			p.goTo(lastRoom);
+		}
 	}
-
-	public Room reward(boolean isCorrect) {
-		return null;
-		
-	}
-
 }
