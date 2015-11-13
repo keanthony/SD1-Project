e364189e9c4da712bfcbc7278274df3b790d7ea9
Adrianna Valdivia <adrianna.valdivia@gmail.com>
2015-11-13 17:55:00 -0500

    fixed the textfield to enter username in GUI

--- DealingWithDeath/src/MAIN/LoginMainGUI.java
+++ DealingWithDeath/src/MAIN/LoginMainGUI.java
@@ -1,6 +1,7 @@
 package MAIN;
 
-import java.awt.TextField;
+
+import javafx.scene.control.TextField;
 import java.util.ArrayList;
 
 import javafx.application.Application;
@@ -33,7 +34,7 @@ public  class LoginMainGUI extends Application {
 				
 				gp.add(new Label("User Name:"), 0, 4);
 				//THIS WILL NOT WORK?!?!?!? WHY!!!!
-				//gp.add(tfUserName = new TextField(), 1, 4);
+				gp.add(tfUserName = new TextField(), 1, 4);
 				
 				Button newGame = new Button("New Game");
 				Button loadGame = new Button("Load Game");
