6b436cb22271390fc93b39c850addf5acf7a77ce
Adrianna Valdivia <adrianna.valdivia@gmail.com>
2015-11-18 14:32:41 -0500

    made NPC serializable

--- DealingWithDeath/src/NPC/NPC.java
+++ DealingWithDeath/src/NPC/NPC.java
@@ -1,7 +1,9 @@
 package NPC;
 
+import java.io.Serializable;
+
 /**Class: NPC.java
-* @author: 
+* @author: Adrianna Valdivia
 * @collaborator: Kevin Anthony, Samuel medina 
 * @version: 1.0
 * Course : ITEC 3860 Fall 2015 Dr. Johnson
@@ -11,7 +13,7 @@ package NPC;
 */
 //Sam: I am going to create a variable for trade. if a player 
 //wants to trade or "deal" with an NPC. We still don't have this.
-public class NPC
+public class NPC implements Serializable
 {
 	protected int npcID;
 	protected String name;
